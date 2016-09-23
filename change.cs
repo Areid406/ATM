647f0aa9f33ccde44c9e4ef74ee744285f82b44d
Areid406 <alexreid302@gmail.com>
2016-09-23 15:48:52 -0600

    Add files via upload
    Initial upload of Max's files

---
+++ change.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atm
+{
+    /*This class handles the amount of change wanted from the ATM*/
+    class change
+    {
+        //The current change being processed
+        private int changeAmt;
+
+        /*Setter to set the current change amount*/
+        void setAmt(int newChange)
+        {
+            changeAmt = newChange;
+        }//setAmt()
+
+        /*Getter to get the current change amount*/
+        int getAmt()
+        {
+            return changeAmt;
+        }//getAmt()
+
+        int main()
+        {
+            denom test = new denom();
+            return 0;
+        }
+    }
+}
