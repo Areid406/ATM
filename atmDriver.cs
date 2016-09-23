70ac8f7e90aaefab1a1ff3d8912cb1785cfceeb0
Areid406 <alexreid302@gmail.com>
2016-09-23 15:36:21 -0600

    Add files via upload
    Uploaded 2 of 5 classes for CSCI305 Lab 4 - ATM

---
+++ atmDriver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atm
+{
+    class atmDriver
+    {
+        static void Main(string[] args)
+        {
+            string filename;
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter a file to open: ");
+                filename = Console.ReadLine();
+            }
+            else
+                filename = args[0];
+
+            atmProcessor process = new atmProcessor(filename);
+            process.go();
+        }
+    }
+}
---
+++ atmProcessor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atm
+{
+    public class atmProcessor
+    {
+        private string filename;
+        public atmProcessor()
+        {}
+
+        public atmProcessor(string filename)
+        {
+            this.filename = filename;
+
+        }
+
+       public void go()
+        {
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader(filename))
+                {
+                    string line;
+                    // Read and display lines from the file until the end of 
+                    // the file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+    
+    
+}
