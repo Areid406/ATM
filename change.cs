7a6c0de80408df8b16b7dc15d2ff6aad8ebd3346
Areid406 <alexreid302@gmail.com>
2016-09-30 11:33:02 -0600

    Update change.cs
    Comments added

--- change.cs
+++ change.cs
@@ -1,3 +1,11 @@
+/* Change.cs -version 0.2 - authored by Alex Reid & Max Austin
+ * For use in CSCI305 Fall 2016 Lab #3 - ATM 
+ * 
+ * Change simply holds the amount of change requested to be dispensed
+ * with appropriate getters and setters.
+ * Will be *hopefully* more useful in later assignments.
+ */
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
